a2aff98f6bb6146ec9d6533addb44994303c69e5
vamshi <vamshis2017@gmail.com>
2023-01-18 23:11:59 +0530

    .

--- shareme_backend/schemas/index.js
+++ shareme_backend/schemas/index.js
@@ -1,1 +1,14 @@
-export const schemaTypes = []
+import createSchema from 'part:@sanity/base/schema-creator'
+
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import user from './user';
+
+export default createSchema({
+    name: 'default',
+    
+    types: schemaTypes.concat([
+        user
+    ]),
+})
+
